f86a243002bc4778784492ea167a9074b949a3e0
Caio sousa <caiyo.projeto@gmail.com>
2021-03-17 20:34:07 -0300

    TokenAuthorization

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,7 +1,13 @@
-﻿using DevIO.Api.ViewModels;
+﻿using DevIO.Api.Extensions;
+using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
@@ -11,13 +17,16 @@ namespace DevIO.Api.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
         public AuthController(INotificador notificador,
                               SignInManager<IdentityUser> signInManager,
-                              UserManager<IdentityUser> userManager) : base(notificador)
+                              UserManager<IdentityUser> userManager,
+                              IOptions<AppSettings> appSettings) : base(notificador)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("nova-conta")]
@@ -38,7 +47,7 @@ namespace DevIO.Api.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return CustomResponse(registerUser);
+                return CustomResponse(GerarJsonWebToken());
             }
             foreach (var error in result.Errors)
             {
@@ -54,14 +63,14 @@ namespace DevIO.Api.Controllers
         {
             if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-            var result = await _signInManager.PasswordSignInAsync(loginUserViewModel.Email, 
+            var result = await _signInManager.PasswordSignInAsync(loginUserViewModel.Email,
                                                                   loginUserViewModel.Password,
                                                                   false,
                                                                   true);
 
             if (result.Succeeded)
             {
-                return CustomResponse(loginUserViewModel);
+                return CustomResponse(GerarJsonWebToken());
             }
 
             if (result.IsLockedOut)
@@ -72,6 +81,24 @@ namespace DevIO.Api.Controllers
 
             NotificarErro("Usuário ou Senha incorretos");
             return CustomResponse(loginUserViewModel);
+        }
+
+        private string GerarJsonWebToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            return encodedToken;
         }
     }
 }
---
+++ src/DevIO.Api/Extensions/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace DevIO.Api.Extensions
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Chave de criptografia do token
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Validade do token
+        /// </summary>
+        public int ExpiracaoHoras { get; set; }
+
+        /// <summary>
+        /// Quem emite
+        /// </summary>
+        public string Emissor { get; set; }
+
+        /// <summary>
+        /// Quais URL o token é valido
+        /// </summary>
+        public string ValidoEm { get; set;}
+    }
+}
