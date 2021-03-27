375cf48078302d8df9e52e11d8888529e7fc21e5
Caio sousa <caiyo.projeto@gmail.com>
2021-03-27 14:31:04 -0300

    Health check V2

--- src/DevIO.Api/Configuration/HealthCheckConfig.cs
+++ src/DevIO.Api/Configuration/HealthCheckConfig.cs
@@ -1,10 +1,11 @@
 ﻿using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DevIO.Api.Configuration
 {
@@ -14,9 +15,7 @@ namespace DevIO.Api.Configuration
         {
             //adding health check services to container
             services.AddHealthChecks()
-                    .AddMongoDb(mongodbConnectionString: Configuration.GetConnectionString("DefaultMongo"),
-                            name: "mongo",
-                            failureStatus: HealthStatus.Unhealthy); //adding MongoDb Health Check
+                   .AddCheck<ExampleHealthCheck>("example_health_check");
 
             //adding healthchecks UI
             services.AddHealthChecksUI(opt =>
@@ -27,32 +26,50 @@ namespace DevIO.Api.Configuration
 
                 opt.AddHealthCheckEndpoint("default api", "/healthz"); //map health check api
             })
-            .AddInMemoryStorage();
+             .AddSqlServerStorage(Configuration.GetConnectionString("DefaultConnection"));
 
             return services;
         }
 
         public static IApplicationBuilder UseHealthCheckConfig(this IApplicationBuilder app)
         {
-            app.UseRouting();
+            app.UseRouting()
+              .UseEndpoints(config =>
+              {
+                  config.MapHealthChecks("/healthz", new HealthCheckOptions
+                  {
+                      Predicate = _ => true,
+                      ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                  });
 
-            app.UseEndpoints(endpoints =>
-            {
-                //adding endpoint of health check for the health check ui in UI format
-                endpoints.MapHealthChecks("/healthz", new HealthCheckOptions
-                {
-                    Predicate = _ => true,
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
+                  config.MapHealthChecksUI(setup =>
+                  {
+                      setup.UIPath = "/show-health-ui"; // this is ui path in your browser
+                      setup.ApiPath = "/health-ui-api"; // the UI ( spa app )  use this path to get information from the store ( this is NOT the healthz path, is internal ui api )
+                  });
 
-                //map healthcheck ui endpoing - default is /healthchecks-ui/
-                endpoints.MapHealthChecksUI();
-
-                endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!"));
-            });
-
+                  config.MapDefaultControllerRoute();
+              });
             return app;
         }
+    }
 
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("An unhealthy result."));
+        }
     }
 }
